1b40fdf293149a133226e1fb62b4a97051a32453
Lucas Lobao <lucaslobaodev@outlook.com>
2024-02-03 18:25:59 -0300

    commit

---
+++ src/main/java/com/lobao/crm/LobaocrmApiApplication.java
@@ -0,0 +1,13 @@
+package com.lobao.crm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LobaocrmApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LobaocrmApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lobao/crm/LobaocrmApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lobao.crm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LobaocrmApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
